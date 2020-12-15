65900be94c7c6cdafdccad6b3c22e797d90c5026
Dorjderem2002 <34859874+Dorjderem2002@users.noreply.github.com>
2020-12-15 10:14:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import Tkinter
+import tkMessageBox
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
+
+
+top = Tkinter.Tk()
+
+def StartSpam():
+	driver.get(URL)
+	link = driver.find_elements_by_class_name("voteDo")
+	button = link[0]
+
+B = Tkinter.Button(top, text ="Start", command = StartSpam)
+
+B.pack()
+top.mainloop()
 
 def get_clear_browsing_button(driver):
     return driver.find_element_by_css_selector('* /deep/ #clearBrowsingDataConfirm')
@@ -24,13 +39,9 @@ driver = webdriver.Chrome(PATH)
 
 URL = "http://bagsh.mn/?t=5fd73a0057f8a9b3e582920b&fbclid=IwAR1Vvdj_GEdK039h9IfXZoW1slkhMM5B9sw2tV2Okb9wj89cs3at8PeNZsk"
 
-driver.get(URL)
 
-link = driver.find_elements_by_class_name("voteDo")
 
-button = link[0]
-
-while True:
+while start:
 	button.click()
 	driver.delete_all_cookies()
 	#there are 2 options
